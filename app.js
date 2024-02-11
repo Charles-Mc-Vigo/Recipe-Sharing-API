0fb1c341f013a45c693b3380c8ed18c612af8713
Charles Mc Vigo <charlesvigo8@gmail.com>
2024-02-11 15:18:28 +0800

    Main

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.get('/api/dessert',(req,res)=>{
     res.send('You are in the DESSERT route')
 })
 
+
+//port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
